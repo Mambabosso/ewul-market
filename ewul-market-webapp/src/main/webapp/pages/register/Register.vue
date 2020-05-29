321cc6e5154613d578fc58508f0d734498ccfc87
Luis Weich <mambabosso@gmail.com>
2020-05-29 18:48:23 +0200

    Update

---
+++ ewul-market-webapp/src/main/webapp/pages/register/Register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="register-form"></div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.register-form {
+  margin: 2px;
+}
+</style>
--- ewul-market-webapp/src/main/webapp/router/routes.js
+++ ewul-market-webapp/src/main/webapp/router/routes.js
@@ -1,6 +1,8 @@
+import Register from "./../pages/register/Register.vue";
 import Login from "./../pages/auth/Login.vue";
 
 export default [
   { path: "/", component: Login },
+  { path: "/register", component: Register },
   { path: "/login", component: Login }
 ];
