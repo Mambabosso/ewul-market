36724127536a2360bd0f8003b5bef164ff5c4380
Luis Weich <mambabosso@gmail.com>
2020-06-10 21:33:58 +0200

    Update

---
+++ ewul-market-server/src/main/java/com/github/mambabosso/ewul/server/model/core/user/SimpleUser.java
@@ -0,0 +1,33 @@
+package com.github.mambabosso.ewul.server.model.core.user;
+
+import com.github.mambabosso.ewul.server.model.core.role.Role;
+import lombok.*;
+
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SimpleUser implements Principal, Serializable {
+
+    private String name;
+    private UserType type;
+    private @Singular Set<String> roles;
+
+    public static SimpleUser from(@NonNull final User user) {
+        String userName = user.getName();
+        UserType userType = user.getType();
+        Set<Role> userRoles = user.getRoles();
+        if (userRoles != null) {
+            Set<String> roles = userRoles.stream().map(Role::getName).collect(Collectors.toSet());
+            return SimpleUser.builder().name(userName).type(userType).roles(roles).build();
+        } else {
+            return SimpleUser.builder().name(userName).type(userType).build();
+        }
+    }
+
+}
